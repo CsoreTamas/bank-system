ddadf20285a54b5a907c232706cce4b23d834e9e
CsoreTamas <csore.tamas123@gmail.com>
2025-08-30 14:07:18 +0200

    remove all files from main

--- src/main/java/org/example/logger/ConsoleLogger.java
+++
@@ -1,8 +0,0 @@
-package org.example.logger;
-
-public class ConsoleLogger implements TransactionLogger {
-    @Override
-    public void log(String massage) {
-        System.out.println(" [LOG] " + massage);
-    }
-}
--- src/main/java/org/example/logger/TransactionLogger.java
+++
@@ -1,5 +0,0 @@
-package org.example.logger;
-
-public interface TransactionLogger {
-    void log(String massage);
-}
